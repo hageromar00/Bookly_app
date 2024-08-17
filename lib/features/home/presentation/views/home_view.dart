bd6e0cb71dd5217786b7d13710c3e2ce0c40ccd6
hageromar <hageromar328@gmail.com>
2024-08-17 19:52:41 +0300

    single responsiplite principle

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -5,6 +5,6 @@ class HomeViews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(body: Text('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),);
   }
 }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/const.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/animate_slide_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,13 +21,19 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
     initSlideAnimate();
-   
+    NavigatorToHome();
+  }
+
+  void NavigatorToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(()=>const HomeViews(),transition: Transition.fade,duration: kTranstionDuration);
+    });
   }
 
   void initSlideAnimate() {
-       animate =
+    animate =
         AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    
+
     slideingAnimation =
         Tween<Offset>(begin: const Offset(0, 14), end: const Offset(0, 0))
             .animate(animate);
@@ -33,11 +42,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
     //   setState(() {});
     // });
   }
+
   @override
   void dispose() {
-    
     super.dispose();
   }
+
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
