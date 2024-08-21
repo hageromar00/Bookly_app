0af7e94dbab87b04e2bd59fee36fadcf4a869a58
hageromar <hageromar328@gmail.com>
2024-08-21 21:28:19 +0300

    create book details

---
+++ lib/features/home/presentation/views/book_details_views.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsViews extends StatelessWidget {
+  const BookDetailsViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: BookDetailViewBody(),);
+  }
+}
