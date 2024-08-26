c9d4228611e5ad312633d827d7d357c20e48b933
hageromar <hageromar328@gmail.com>
2024-08-27 00:14:49 +0300

    add chached network image to list book horizontal

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,24 +1,35 @@
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key,required this.imgurl});
+  const CustomBookImage({super.key, required this.imgurl});
   final String imgurl;
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4, //width/height
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color:  Color.fromARGB(255, 231, 199, 199),
-            image:  DecorationImage(
-               fit: BoxFit.fill,
-                image:NetworkImage(imgurl)
-                //  AssetImage(AssetsDate.testImage)
-                ,
-                ),)
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4, //width/height
+        child: CachedNetworkImage(
+          imageUrl: imgurl,
+          errorWidget: (context, url, error) =>
+              const Icon(FontAwesomeIcons.circleExclamation),
+          placeholder: (context, url) =>
+              const Center(child: CircularProgressIndicator()),
+          fit: BoxFit.fill,
+        ),
+        // child: Container(
+        //     decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(16),
+        //   image: DecorationImage(
+        //     fit: BoxFit.fill,
+        //     image: NetworkImage(imgurl)
+        //     //  AssetImage(AssetsDate.testImage)
+        //     ,
+        //   ),
+        // )),
       ),
     );
   }
